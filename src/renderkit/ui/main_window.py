754f005b6ed45781ea8a810a9a27c9447945a98d
Ahmed Hindy <Ahmed.hindy96@gmail.com>
2026-01-15 15:11:37 +0200

    re-added QMessageBox

--- src/renderkit/ui/main_window.py
+++ src/renderkit/ui/main_window.py
@@ -23,6 +23,7 @@ from renderkit.ui.main_window_widgets import UiLogForwarder
 from renderkit.ui.qt_compat import (
     QApplication,
     QMainWindow,
+    QMessageBox,  # noqa: F401
     QSettings,
     QSplitter,
     QSystemTrayIcon,
